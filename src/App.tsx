e20dcb06a25ef9cf29dfcdaf644bab52e6ed4d25
Hau <hau.td@reecorp.com>
2023-11-15 09:40:31 +0700

    uploaded success

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,18 @@ type RowCol = { rows: Row[]; columns: AOAColumn[]; };
 
 function arrayify(rows: any[]): Row[] {
   return rows.map(row => {
-    if(Array.isArray(row)) return row;
+    if (Array.isArray(row)) return row;
     var length = Object.keys(row).length;
-    for(; length > 0; --length) if(row[length-1] != null) break;
-    return Array.from({length, ...row});
+    for (; length > 0; --length) if (row[length - 1] != null) break;
+    return Array.from({ length, ...row });
   });
 }
 
 /* this method returns `rows` and `columns` data for sheet change */
-const getRowsCols = ( data: DataSet, sheetName: string ): RowCol => ({
-  rows: utils.sheet_to_json<Row>(data[sheetName], {header:1}),
+const getRowsCols = (data: DataSet, sheetName: string): RowCol => ({
+  rows: utils.sheet_to_json<Row>(data[sheetName], { header: 1 }),
   columns: Array.from({
-    length: utils.decode_range(data[sheetName]["!ref"]||"A1").e.c + 1
+    length: utils.decode_range(data[sheetName]["!ref"] || "A1").e.c + 1
   }, (_, i) => ({ key: String(i), name: utils.encode_col(i), editor: textEditor }))
 });
 
@@ -33,6 +33,7 @@ export default function App() {
   const [workBook, setWorkBook] = useState<DataSet>({} as DataSet); // workbook
   const [sheets, setSheets] = useState<string[]>([]); // list of sheet names
   const [current, setCurrent] = useState<string>(""); // selected sheet
+  const [file, setFile] = useState<File | null>(null); // store the uploaded file
 
   /* called when sheet dropdown is changed */
   function selectSheet(name: string) {
@@ -64,16 +65,31 @@ export default function App() {
   }
 
   /* called when file input element is used to select a new file */
+  // async function handleFile(ev: ChangeEvent<HTMLInputElement>): Promise<void> {
+  //   const file = await ev.target.files?.[0]?.arrayBuffer();
+  //   if(file) {
+  //     await handleAB(file)
+  //   } 
+  // }
+
   async function handleFile(ev: ChangeEvent<HTMLInputElement>): Promise<void> {
-    const file = await ev.target.files?.[0]?.arrayBuffer();
-    if(file) await handleAB(file);
+    const selectedFile = ev.target.files?.[0];
+    if (selectedFile) {
+      const file = await selectedFile.arrayBuffer();
+      await handleAB(file);
+      setFile(selectedFile);
+    } else {
+      console.error("No file selected.");
+    }
   }
 
   /* when page is loaded, fetch and processs worksheet */
-  useEffect(() => { (async () => {
+  useEffect(() => {
+    (async () => {
       const f = await fetch("https://sheetjs.com/pres.numbers");
       await handleAB(await f.arrayBuffer());
-  })(); }, []);
+    })();
+  }, []);
 
   /* method is called when one of the save buttons is clicked */
   function saveFile(ext: string): void {
@@ -89,25 +105,48 @@ export default function App() {
     writeFile(wb, "SheetJSRDG." + ext);
   }
 
+  // async function uploadToDatabase() {
+  //   try {
+  //     const formData = new FormData();
+
+  //     formData.append("excelFile", new File([current], ''));
+
+  //     const response = await fetch("http://localhost:3000/import-excel", {
+  //       method: "POST",
+  //       body: formData,
+  //     });
+
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       console.log("Upload success:", data);
+  //     } else {
+  //       console.error("Upload failed:", response.statusText);
+  //     }
+  //   } catch (error) {
+  //     console.error("Upload error:", error);
+  //   }
+  // }
+
   async function uploadToDatabase() {
     try {
-      const formData = new FormData();
-      // const excelFile = new Blob([writeFile(data: workBook, filename: fileURLToPath, opts: )], {
-      //   type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-      // });
-  
-      formData.append("excelFile", new File([current], ''));
-  
-      const response = await fetch("http://localhost:3000/import-excel", {
-        method: "POST",
-        body: formData,
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        console.log("Upload success:", data);
+      if (file) {
+        const formData = new FormData();
+        formData.append("excelFile", file);
+
+        const response = await fetch("http://localhost:3000/import-excel", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Upload success:", data);
+          alert('CHÚC MỪNG GẤU ĐỎ')
+        } else {
+          console.error("Upload failed:", response.statusText);
+        }
       } else {
-        console.error("Upload failed:", response.statusText);
+        console.error("No file selected.");
       }
     } catch (error) {
       console.error("Upload error:", error);
@@ -117,9 +156,9 @@ export default function App() {
   return (
     <>
       <h3>SheetJS × React-Data-Grid Demo</h3>
-      <div><h1>CÁI COIN CARD</h1></div>
+      <h1 style={{textAlign: 'center'}}>HẾ LÔ MẤY CƯNG</h1>
       <input type="file" onChange={handleFile} />
-      {sheets.length > 0 && ( <>
+      {sheets.length > 0 && (<>
         <p>Use the dropdown to switch to a worksheet:&nbsp;
           <select onChange={async (e) => selectSheet(sheets[+(e.target.value)])}>
             {sheets.map((sheet, idx) => (<option key={sheet} value={idx}>{sheet}</option>))}
@@ -129,11 +168,11 @@ export default function App() {
         <DataGrid columns={columns} rows={rows} onRowsChange={setRows} />
         <p>Click one of the buttons to create a new file with the modified data</p>
         <div><button onClick={() => uploadToDatabase()}>Upload to database</button></div>
-        <div className="flex-cont">{["xlsx", "xlsb", "xls"].map((ext) => (
+        {/* <div className="flex-cont">{["xlsx", "xlsb", "xls"].map((ext) => (
           <button key={ext} onClick={() => saveFile(ext)}>export [.{ext}]</button>
-        ))}</div>
-        
-      </> )}
+        ))}</div> */}
+
+      </>)}
     </>
   );
 }
